a7ae23786d042f6443fc25647a4bb99e58aab853
zakaria kharroub <kharroubzakaria32@gmail.com>
2025-05-08 17:12:40 +0100

    archet

---
+++ src/app/pages/home/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from '../../../shared/UI/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
